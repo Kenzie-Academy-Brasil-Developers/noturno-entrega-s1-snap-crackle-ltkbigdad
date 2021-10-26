a938e2c700bcdebfa2e44a7142cb3c89515b50c9
Douglas Braz Batista <doug.batista@live.com>
2021-10-26 19:53:19 -0300

    src/snapCrackle.js

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,21 @@
-// Seu código vai aqui
+function snapCrackle(maxValue){
+  for (let contador = 1; contador <= maxValue; contador++){   
+   
+    
+     if(contador % 5 == 0 && contador % 2 == 1){
+     let num = 'snapCrackle'
+     console.log(num)
+    }
+   else if(contador % 5 == 0){
+     let num = 'crackle'
+     console.log(num) 
+   } 
+    else if(contador % 2 == 1){
+     let num = 'snap'
+     console.log(num)
+   }  
+    else{console.log(contador)}
+    
+  }
+}
+
